557428c57911b86fb2a501203d249caf6a56e453
leesam <toyboy2@hanmail.net>
2025-07-01 23:05:25 +0900

    feat: increse debound for icloud notification

--- Projects/App/Sources/ViewModels/MainViewModel.swift
+++ Projects/App/Sources/ViewModels/MainViewModel.swift
@@ -15,7 +15,7 @@ class MainViewModel: ObservableObject {
         self.storageManager = storageManager
         // NSManagedObjectContextDidSave Notification 구독
         storageManager.remoteChangePublisher
-            .debounce(for: .milliseconds(100), scheduler: DispatchQueue.main)
+            .debounce(for: .seconds(1), scheduler: DispatchQueue.main)
             .receive(on: DispatchQueue.main)
             .sink { [weak self] (notification) in
                 print("WordSetManager.remomteChange. \(notification.userInfo)")
